2776658d22581b6a1736182fb9737f2fe520d09c
kasanetetouwu0401 <kasanetetouwu0401@gmail.com>
2026-02-02 11:19:50 +0700

    Last Sync: 2026-02-02 11:19 (Mobile)

--- _worker.js
+++ _worker.js
@@ -16,8 +16,8 @@ const v2 = "djJyYXk=";
 const PORTS = [443, 80];
 const PROTOCOLS = [atob(horse), atob(flash), atob(neko), "ss"];
 const SUB_PAGE_URL = "https://foolvpn.web.id/nautica";
-const KV_PRX_URL = "https://raw.githubusercontent.com/kasanetetouwu0401/uwu/refs/heads/main/kvProxyList.json";
-const PRX_BANK_URL = "https://raw.githubusercontent.com/kasanetetouwu0401/uwu/refs/heads/main/proxyList.txt";
+const KV_PRX_URL = "https://raw.githubusercontent.com/FoolVPN-ID/Nautica/refs/heads/main/kvProxyList.json";
+const PRX_BANK_URL = "https://raw.githubusercontent.com/FoolVPN-ID/Nautica/refs/heads/main/proxyList.txt";
 const DNS_SERVER_ADDRESS = "8.8.8.8";
 const DNS_SERVER_PORT = 53;
 const RELAY_SERVER_UDP = {
